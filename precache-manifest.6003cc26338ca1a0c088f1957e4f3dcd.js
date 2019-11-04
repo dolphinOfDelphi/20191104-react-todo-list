aec0f37b95cffe32ff94d141ff067c67946b5afe
Daniel <caudaDelphini@gmail.com>
2019-11-04 18:32:23 -0500

    Updates

---
+++ precache-manifest.6003cc26338ca1a0c088f1957e4f3dcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d242272d193f00ff32f624a78cc52ce",
+    "url": "/20191104-react-todo-list/index.html"
+  },
+  {
+    "revision": "2a6726aaec3c6f36811f",
+    "url": "/20191104-react-todo-list/static/css/main.88204db0.chunk.css"
+  },
+  {
+    "revision": "f05211ff0cb45a24242b",
+    "url": "/20191104-react-todo-list/static/js/2.ee4df55c.chunk.js"
+  },
+  {
+    "revision": "2a6726aaec3c6f36811f",
+    "url": "/20191104-react-todo-list/static/js/main.8048732f.chunk.js"
+  },
+  {
+    "revision": "5e9d465c3c35031dfdb7",
+    "url": "/20191104-react-todo-list/static/js/runtime-main.029fdb46.js"
+  }
+]);
