293ce8343c437031e7fc9b116d74e62181184d76
Daniel <caudadelphini@gmail.com>
2019-11-14 16:28:15 -0500

    Adjust responsive sizing

--- src/Queries.js
+++ src/Queries.js
@@ -9,49 +9,55 @@ export const Query = (props) =>
     />;
 export const SortForm = (props) =>
     <form>
-        <input
-            id='name'
-            className='sort'
-            type='radio'
-            name='sort-by'
-            value='name'
-            checked={props.sortBy === 'name'}
-            onChange={props.handleChange}
-        />
-        <label
-            className='sort'
-            htmlFor='name'
-        >
-            Name
-        </label>
-        <input
-            id='due-date'
-            className='sort'
-            type='radio'
-            name='sort-by'
-            value='dueDate'
-            checked={props.sortBy === 'dueDate'}
-            onChange={props.handleChange}
-        />
-        <label
-            className='sort'
-            htmlFor='due-date'
-        >
-            Due Date
-        </label>
-        <input
-            id='priority'
-            className='sort'
-            type='radio'
-            name='sort-by'
-            value='priority'
-            checked={props.sortBy === 'priority'}
-            onChange={props.handleChange}
-        />
-        <label
-            className='sort'
-            htmlFor='priority'
-        >
-            Priority
-        </label>
+        <div style={{display: 'inline-block'}}>
+            <input
+                id='name'
+                className='sort'
+                type='radio'
+                name='sort-by'
+                value='name'
+                checked={props.sortBy === 'name'}
+                onChange={props.handleChange}
+            />
+            <label
+                className='sort'
+                htmlFor='name'
+            >
+                Name
+            </label>
+        </div>
+        <div style={{display: 'inline-block'}}>
+            <input
+                id='due-date'
+                className='sort'
+                type='radio'
+                name='sort-by'
+                value='dueDate'
+                checked={props.sortBy === 'dueDate'}
+                onChange={props.handleChange}
+            />
+            <label
+                className='sort'
+                htmlFor='due-date'
+            >
+                Due Date
+            </label>
+        </div>
+        <div style={{display: 'inline-block'}}>
+            <input
+                id='priority'
+                className='sort'
+                type='radio'
+                name='sort-by'
+                value='priority'
+                checked={props.sortBy === 'priority'}
+                onChange={props.handleChange}
+            />
+            <label
+                className='sort'
+                htmlFor='priority'
+            >
+                Priority
+            </label>
+        </div>
     </form>;
